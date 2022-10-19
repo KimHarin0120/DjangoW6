4f08874a32d02e418b1259608fbcf136a831dbbd
Kimharin0120 <priincessharin27@gmail.com>
2022-10-19 17:52:23 +0900

    DjangoW6

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+# [코드 작성] models.py의 Posting 모델 불러오기
+from .models import Weapon
 
 # Register your models here.
+# [코드 작성] Posting 모델 등록
+admin.site.register(Weapon)
